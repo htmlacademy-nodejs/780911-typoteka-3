6cc55661dd8d60f37216cf2ae8be8b61ebe9674d
Olga Shitova <olgashitova@MacBook-Pro-2.local>
2021-02-01 11:23:04 +0200

    final

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -9,61 +9,34 @@ const {
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const TITLES = `./data/titles.txt`;
+const ANNOUNCE = `./data/sentences.txt`;
+const CATEGORIES = `./data/categories.txt`;
 
-const ANNOUNCE = [
-  "Ёлки — это не просто красивое дерево. Это прочная древесина. Первая большая ёлка была установлена только в 1938 году.",
-  "Вы можете достичь всего. Стоит только немного постараться и запастись книгами.",
-  "Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.",
-  "Золотое сечение — соотношение двух величин, гармоническая пропорция.",
-  "Собрать камни бесконечности легко, если вы прирожденный герой.",
-  "Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.",
-  "Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.",
-  "Программировать не настолько сложно, как об этом говорят. Простые ежедневные упражнения помогут достичь успеха.",
-  "Это один из лучших рок-музыкантов.Он написал больше 30 хитов.Из под его пера вышло 8 платиновых альбомов.",
-  "Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.",
-  "Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?",
-  "Достичь успеха помогут ежедневные повторения.Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.Как начать действовать? Для начала просто соберитесь.",
-  "Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.",
-  "Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.",
-];
+const readContent = async (filePath) => {
 
-const CATEGORIES = [
-  "Деревья",
-  "За жизнь",
-  "Без рамки",
-  "Разное",
-  "IT",
-  "Музыка",
-  "Кино",
-  "Программирование",
-  "Железо",
-];
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(/\n|\r/g).filter((item) => {
+      return item.length > 0;
+    });
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
-const generatePublications = (count) =>
+const generatePublications = (count, titles, categories, sentences) =>
   Array(count)
     .fill({})
     .map(() => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
-      announce: shuffle(ANNOUNCE).slice(1, 5).join(` `),
-      fullText: shuffle(ANNOUNCE)
-        .slice(1, getRandomInt(0, ANNOUNCE.length - 1))
+      title: titles[getRandomInt(0, titles.length - 1)],
+      announce: shuffle(sentences).slice(1, 5).join(` `),
+      fullText: shuffle(sentences)
+        .slice(1, getRandomInt(0, sentences.length - 1))
         .join(` `),
       createdDate: getRandomDateOfLastThreeMonths(),
-      category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+      category: [categories[getRandomInt(0, categories.length - 1)]],
     }));
 
 module.exports = {
@@ -71,8 +44,12 @@ module.exports = {
   async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublications(countOffer));
-
+    const titles = await readContent(TITLES);
+    const categories = await readContent(CATEGORIES);
+    const sentences = await readContent(ANNOUNCE);
+    const content = JSON.stringify(
+      generatePublications(countOffer, titles, categories, sentences)
+    );
 
     try {
       await fs.writeFile(FILE_NAME, JSON.stringify(content));
@@ -82,6 +59,3 @@ module.exports = {
     }
   },
 };
-
-
-
