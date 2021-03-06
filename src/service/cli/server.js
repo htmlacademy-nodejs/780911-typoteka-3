720c113a8522fed5e4283eb3d6fcc2bb0ac47445
Olga Shitova <olgashitova@MacBook-Pro-2.local>
2021-03-06 21:51:44 +0200

    add express and router to server file

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,11 +1,14 @@
 "use strict";
-const http = require(`http`);
+
 const DEFAULT_PORT = 3000;
 const MOCK_FILE_PATH = `./mocks.json`;
 const fs = require(`fs`).promises;
 const fs2 = require(`fs`);
 const chalk = require(`chalk`);
-const {HttpCode} = require(`../../HttpCode`);
+const { HttpCode } = require(`../../HttpCode`);
+const { Router } = require(`express`);
+const postsRouter = new Router();
+const express = require(`express`);
 
 const readContent = async (filePath) => {
   try {
@@ -56,39 +59,40 @@ const returnTitles = async (file) => {
   }
 };
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const titlesList = await returnTitles(MOCK_FILE_PATH);
-        const message = titlesList.map((post) => `<li>${post}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, err, req, res);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
-
 module.exports = {
   name: `--server`,
   async run(args) {
     const port = args ? Number.parseInt(args[0], 10) : DEFAULT_PORT;
+    const notFoundMessageText = `Not found`;
+    const titlesList = await returnTitles(MOCK_FILE_PATH);
+    const message = titlesList.map((post) => `<li>${post}</li>`).join(``);
+    const app = express();
 
-    http
-      .createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(`Ошибка при создании сервера`, err);
-        }
-        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      });
+    app.get(`/`, async (req, res) => {
+      try {
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, err, req, res);
+      }
+    });
+
+    app.use(
+      `/posts`,
+      postsRouter.get(`/`, async (req, res) => {
+        const jsonRes = JSON.parse(await fs.readFile(MOCK_FILE_PATH, `utf8`));
+        res.json(jsonRes);
+      })
+    );
+
+    app.use(function (req, res) {
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+    });
+
+    app.listen(port, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`, err);
+      }
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    });
   },
 };
