18c7b124900687e6ee729c8f7a2d77a8b99210cb
Olga Shitova <olgashitova@MacBook-Pro-2.local>
2021-08-23 22:32:35 +0300

    add sequilize file

---
+++ src/service/lib/sequelize.js
@@ -0,0 +1,47 @@
+"use strict";
+require("dotenv").config();
+const Sequelize = require(`sequelize`);
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+// const somethingIsNotDefined = [
+//   DB_NAME,
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_HOST,
+//   DB_PORT,
+// ].some((it) => it === undefined);
+const notDefinedENVVars = [
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+].filter((envVar) => envVar === undefined);
+
+if (notDefinedENVVars.length > 0) {
+  throw new Error(
+    `One or more environmental variables are not defined: ${notDefinedENVVars.join(
+      ", "
+    )}`
+  );
+}
+
+// if (somethingIsNotDefined) {
+//   throw new Error(`One or more environmental variables are not defined`);
+// }
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: `postgres`,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 10000,
+    idle: 10000,
+  },
+});
+
+module.exports = {
+  sequelize,
+};
